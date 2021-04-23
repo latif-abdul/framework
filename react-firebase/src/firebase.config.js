71d73deecb5d0f6bced4027e0e3312f5ddb972d1
latif-abdul <1841720110@student.polinema.ac.id>
2021-04-23 10:51:19 +0700

    Modul 10

---
+++ react-firebase/src/firebase.config.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyD_R2jS8t2dzFBurj2IF6oIaHlMN2snBTc",
+  authDomain: "react-firebase-ff1dc.firebaseapp.com",
+  projectId: "react-firebase-ff1dc",
+  storageBucket: "react-firebase-ff1dc.appspot.com",
+  messagingSenderId: "37936227075",
+  appId: "1:37936227075:web:41efdad6ab3eabfe8a3f36",
+  measurementId: "G-QJWG1PWQR5"
+};
+export default firebaseConfig;
