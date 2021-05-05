b8e085a8be4e8ab7cb480701a6fa6b4c56ef9def
latif-abdul <1841720110@student.polinema.ac.id>
2021-05-05 18:35:23 +0700

    pertemuan 12

---
+++ laravelapp/resources/views/biodata.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Biodata Mahasiswa</title>
+    </head>
+    <body>
+        <h3>Nama : Abdul Latif</h3>
+        <h3>NIM : 1841720110</h3>
+        <h3>Kelas : TI-3F</h3>
+        <h3>Alamat : Ciptomulyo</h3>
+    </body>
+</html>
